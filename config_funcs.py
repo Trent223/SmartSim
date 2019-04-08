ed234d8362e70204aea8a94b7acb819ea1935036
jbsturgill1 <31523310+jbsturgill1@users.noreply.github.com>
2019-04-08 11:21:19 -0400

    Add files via upload

--- config_funcs.py
+++ config_funcs.py
@@ -14,9 +14,30 @@ try:
     #Attempt to import the given config file
     config_file = __import__(command)
 except:
-    #If given config file could not be loaded report an error and close application
-    print("Could not import "+command)
-    sys.exit()
+    #If given config file could not be loaded walk user through creating a config file JBS
+    config_file = input("Config file could not be imported. Enter name of config file to be created: ")
+    #Create config file JBS
+    config_name = config_file+'.txt'
+    f = open(config_name, 'a+')
+    #initialize value for add_equation JBS
+    add_equation = 'y'
+    while add_equation == 'y':
+        equation_name = input("Enter name of equation: ")
+        f.write("%s = { \n" % equation_name)
+        add_field = input("Would you like to add a field? Enter y or n: ")
+        while add_field == 'y':
+            field_name = input("Enter name of field: ")
+            f.write("     %s : " %field_name)
+            field_value = input("Enter field value: ")
+            f.write("%s \n" % field_value)
+            add_field = input("Add another field? Enter y or n: ")
+        f.write("} \n")
+        add_equation = input("Would you like to add another equation? Enter y or n: ")
+    f.close()
+    
+    
+    
+    
 
 def get_optimizer_values(metric):
 
