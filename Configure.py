18543af47bafdaf44ef31be0f15baf2770e92cd4
Trent223 <trent.minch@yahoo.com>
2019-04-11 20:26:04 -0400

    Added more necessary files and code for config file replacement

--- Configure.py
+++ Configure.py
@@ -1,5 +1,5 @@
 '''
-Author: Trent Minch
+Author(s): Trent Minch
 Purpose: Generate the SmartSim config file 
 by reading from the available yaml files in the 
 current directory and asking the user to provide
@@ -14,6 +14,7 @@ class ConfigData():
     Holds all the relevant data to generate the program.
     This is the data that used to be stored in config_file.py
     '''
+    
     #Actual config file data
     metrics = []
     #Yaml files in the CWD that are useful to us, and their 
@@ -35,7 +36,9 @@ class ConfigData():
         "SecondaryCornersKeyword" : "secondary corners",
         "HeadersKeyword" : "headers",
         }
-    
+    #These are the keys that must be in ConfigData for each 
+    #Metric in order for the program to run
+    RequiredKeys = ["Model", "Xaxis", "DesignParams", "DevsimParams", "OptimizerParams", "ModelsPath", "Corners", "SecondaryCorners", "Headers"]
 
 
 def ScanCurrentDirectory():
@@ -73,7 +76,28 @@ def ScanCurrentDirectory():
                 continue
             else:
                 print("Found valid file {} for config file field {}".format(file, field))
+                #If we have multiple files for one field, we want to store them together TODO
                 ConfigData.YamlFiles.append({"Field" : field, "File" : file})
+    
+def ParseFoundYamlFiles():
+    '''
+    Author: Trent Minch
+    After the valid config files in the working
+    directory are identified, go through them and 
+    perform the following two actions
+    
+    1. If the yaml configs have only one possible value
+    for the field, store that value
+    2. Otherwise, ask the user which one to use
+    '''
+    for file in ConfigData.YamlFiles:
+        with open(file, "r") as YFile: 
+            Yaml = yaml.safe_load(YFile)
+        TopLevelKeys = Yaml.keys()
+        for key in TopLevelKeys:
+            
+    
+    
     
 def VerifyConfigData():
     
@@ -89,10 +113,57 @@ def VerifyConfigData():
     '''
     #Start true. If any required field is missing, switch to false and return
     ConfigIsGood = True
-    
-
+    for i in range(len(ConfigData.metrics)):
+        CurrentMetric = list(ConfigData.metrics[i].keys())[0]
+        CurrentMetricDataKeys = list(ConfigData.metrics[i][CurrentMetric].keys())
+        for key in ConfigData.RequiredKeys:
+            if key not in CurrentMetricDataKeys:
+                print("Required key {} not in Metric {}".format(key, CurrentMetric))
+                ConfigIsGood = False
     
     return ConfigIsGood
+
+def UpdateConfigData(Metric, Field, Value):
+    '''
+    Author: Trent Minch
+    Updates the data in ConfigData with provided data
+    Arguments: Metric, Field, Value
+    
+    Metric: Which metric in ConfigData.metrics to update.
+    If this Metric isn't in ConfigData already, add it
+    
+    Field: Which field in the Metric's config data to update. If the field
+    isn't already in the config data for that metric, add it.
+    
+    Value: The value corresponding to the given field
+    
+    Returns: nothing
+    '''
+    #If the metric is not already in the config file, add it
+    MetricExists = False
+    for item in ConfigData.metrics:
+        if Metric in item:
+            MetricExists = True
+    if not MetricExists:
+        ConfigData.metrics.append({Metric : {}})
+    #Within that metric, if the field doesn't exist, add it
+    #Otherwise, update it
+    MetricIndex = 0
+    for item in ConfigData.metrics:
+        if Metric in item.keys():
+            MetricIndex = ConfigData.metrics.index(item)
+    
+    ConfigData.metrics[MetricIndex][Metric].update({Field, Value})
+    
+def ConfigSetup():
+    '''
+    Author: Trent Minch
+    Calls other functions in this file to set up the config file
+    '''
+    ScanCurrentDirectory()
+    #Add all the currently known data into ConfigData
+    for 
+    
 
 def main():
     '''
